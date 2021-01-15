9cff29a4cf4e7ce06b8e26a65aa3558cead92846
Furtif <msntchat@hotmail.fr>
2021-01-15 17:09:42 +0100

    add 197 base

--- compile_base.py
+++ compile_base.py
@@ -9,7 +9,7 @@ import shutil
 from subprocess import call
 
 # Variables
-global_version = '0.195.2'
+global_version = '0.197.0'
 protoc_executable = "protoc"
 package_name = 'POGOProtos.Rpc'
 input_file = "POGOProtos.Rpc.proto"
